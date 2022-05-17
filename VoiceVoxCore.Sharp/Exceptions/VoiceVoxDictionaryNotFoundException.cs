3d4b096450532a566b25ebeaeef4696a637dd2db
Algo <corpse2party@gmail.com>
2022-05-17 22:52:41 +0900

    [FIX]ファイル分割忘れ

--- VoiceVoxCore.Sharp/Exceptions/VoiceVoxDictionaryNotFoundException.cs
+++ VoiceVoxCore.Sharp/Exceptions/VoiceVoxDictionaryNotFoundException.cs
@@ -23,23 +23,4 @@ namespace VoiceVoxCore.Sharp.Exceptions
         {
         }
     }
-
-    public class VoiceVoxVersionNotFoundException : Exception
-    {
-        public VoiceVoxVersionNotFoundException()
-        {
-        }
-
-        public VoiceVoxVersionNotFoundException(string message) : base(message)
-        {
-        }
-
-        public VoiceVoxVersionNotFoundException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
-
-        protected VoiceVoxVersionNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
-    }
 }
---
+++ VoiceVoxCore.Sharp/Exceptions/VoiceVoxVersionNotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace VoiceVoxCore.Sharp.Exceptions
+{
+    public class VoiceVoxVersionNotFoundException : Exception
+    {
+        public VoiceVoxVersionNotFoundException()
+        {
+        }
+
+        public VoiceVoxVersionNotFoundException(string message) : base(message)
+        {
+        }
+
+        public VoiceVoxVersionNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected VoiceVoxVersionNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
